a2f56d2389feb940b211c950bcc43d5c511b806f
SvetaSeh <sehsveta13@gmail.com>
2025-02-03 23:18:57 +0100

    arifmetics

---
+++ hw_3-firstJSproj/src/arifmetics.js
@@ -0,0 +1,71 @@
+//остача від ділення
+const x = 2;
+const y = 5;
+
+let z = y%x;
+console.log(z);
+
+//додавання stringa числу
+
+const stringVar = 'book';
+const k = 145;
+let price = stringVar + k;
+
+console.log(stringVar, k);
+console.log(price);
+
+//Boolean and null
+
+const boolVar = false;
+let w = null;
+
+console.log(boolVar, w);
+
+//string text
+
+const first = 'Life';
+const second = 'Is';
+const third = 'Beautiful';
+
+let text = first + second + third;
+
+console.log(text);
+
+//Object
+
+const obj1 = Number ({
+    v1: '12',
+    v2: 19
+});
+const obj2 = String ({
+    s1: 'best',
+    s2: 'stay'
+});
+
+console.log(obj1 / obj2);
+
+console.log(price, obj2);
+
+//інкремент та декремент
+
+let r = 14;
+r++;
+console.log(r);
+
+let t=5;
+t --;
+console.log(t);
+
+//модифікація та присвоєння
+
+let p = 19;
+p += 2;
+console.log(p);
+
+let m = 63;
+m -= 64;
+console.log(r); //навмисна помилка
+
+let u = 145;
+u *= 4;
+console.log(u);
