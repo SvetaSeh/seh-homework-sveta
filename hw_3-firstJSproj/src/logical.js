e959b2337ca4e0019308287f46ed32080cdf148a
SvetaSeh <sehsveta13@gmail.com>
2025-02-04 01:33:03 +0100

    decision-tree

--- hw_3-firstJSproj/src/logical.js
+++ hw_3-firstJSproj/src/logical.js
@@ -2,25 +2,25 @@
 const a1=2;
 const a2=3;
 const a3='2';
-    console.log(a1<a2, a1>a2);
-    console.log(a1>=a2, a1<=a2);
-    console.log(a1==a2);
-    console.log(a1===a3);
-    console.log(a1 != a2);
-    console.log(a1 !== a2);
+console.log(a1<a2, a1>a2);
+console.log(a1>=a2, a1<=a2);
+console.log(a1==a2);
+console.log(a1===a3);
+console.log(a1 != a2);
+console.log(a1 !== a2);
 
 const red1 = '23';
 const red2 = 23;
 
-    console.log(red1>=red2);
-    console.log(red1 != red2);
-    console.log(red1 !== red2);
+console.log(red1>=red2);
+console.log(red1 != red2);
+console.log(red1 !== red2);
 
 const word = 'summer';
 const d = true;
 
-    console.log(word == d);
-    console.log(word === d);
+console.log(word == d);
+console.log(word === d);
 
 
 //Logical Operators
