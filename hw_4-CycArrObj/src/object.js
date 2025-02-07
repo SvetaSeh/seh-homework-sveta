320bc17f6246d8f8f29e15247f074c307db06eb2
SvetaSeh <sehsveta13@gmail.com>
2025-02-07 20:36:35 +0100

    hw 4 object

---
+++ hw_4-CycArrObj/src/object.js
@@ -0,0 +1,43 @@
+const newMenu = {
+    name: 'Central Pub',
+    location: 'London, England',
+    weekMenu: function() {
+        console.log(this.name, this.location);
+    },
+    menuMonday: [
+        {
+            food: 'Fish and chips',
+            drink: 'Mineral water',
+            dessert: 'Brownie'
+        },
+        {
+            food: 'Shepherds pie',
+            drink: 'Cappuccino',
+            dessert: 'Fondant'
+        }
+    ],
+    mondayMenu: function() {
+        console.log(this.menuMonday)
+    },
+
+    menuTuesday:[
+        {
+            food: 'Beef steak',
+            drink: 'Orange juice',
+            dessert: 'Oreo cheesecake'
+        },
+        {
+            food: 'Schnitzel',
+            drink: 'Apple juice',
+            dessert: 'Charlotte'
+        },
+
+    ],
+    tuesdayMenu: function() {
+        console.log(this.menuTuesday)
+    },
+};
+
+newMenu.weekMenu(this.name, this.location);
+newMenu.mondayMenu(this.menuMonday);
+newMenu.tuesdayMenu(this.menuTuesday);
