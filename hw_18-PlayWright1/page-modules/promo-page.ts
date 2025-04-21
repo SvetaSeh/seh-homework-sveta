4c6a665b0ac5e44da4fd08c659d87c8f91868638
SvetaSeh <sehsveta13@gmail.com>
2025-04-21 16:11:55 +0200

    changes

--- hw_18-PlayWright1/page-modules/promo-page.ts
+++ hw_18-PlayWright1/page-modules/promo-page.ts
@@ -1,33 +1,35 @@
-import { BrowserContext, Locator, Page} from "@playwright/test";
+import { BrowserContext, Locator, Page, expect} from "@playwright/test";
 
 
 export class PromoPage {
-    private page: Page;
-
-    public constructor(page: Page) {
-        this.page = page;
-    }
-
+   
     public goTo(): void {
         this.page.goto('https://prom.ua/ua/');
     }
-}
+    
+    public constructor(private page: Page) {}
 
-export class PromoPageElements {
-    public get searchInput(): Locator {
-        return this.page.locator('[data-qaid="search_input"] input');
+    public get searchForm(): Locator {
+        return this.page.locator('div [data-qaid="search_form"]');
     }
 
+    public get searchInput(): Locator {
+        return this.page.locator('input[placeholder="Я шукаю..."]');}
+
     public get searchButton(): Locator {
-        return this.page.locator('[data-qaid="search_btn"] button');
+        return this.page.locator('button [data-qaid="search_btn"]');
     }
 
     public get searchResultGallery(): Locator {
         return this.page.locator('div [data-qaid="product_gallery"]');
     }
 
+    public get productBlock(): Locator {
+        return this.page.locator('div [data-qaid="product_block"]');
+    }
+
     public get firstProductListing(): Locator {
-        return this.page.locator('div [data-position-qaid="1"]');
+        return this.page.locator('div [class="JicYY"] div [data-position-qaid="1"]');
     }
 
     public get buyButton(): Locator {
@@ -37,24 +39,19 @@ export class PromoPageElements {
     public get shoppingCartButton(): Locator {
         return this.page.locator('button [data-qaid="shopping_button"]');
     }
-    public constructor(private page: Page, private context: BrowserContext) {}
 
     public async inputValue(query: string): Promise<void> {
-        await this.searchInput.waitFor({ state: 'visible' });
-        await this.searchInput.click();
+        await this.searchForm.click({ force: true });
         await this.searchInput.fill(query);
-        await this.searchButton.click({ force: true });
-        await this.page.waitForSelector('.searchResultGallery');
+        await this.searchInput.press('Enter');
     }
 
-    public async addToCart(): Promise<void> {
-        await this.firstProductListing.waitFor({ state: 'visible' });
+    public async addToCart(query: string): Promise<void> {
+        await this.searchForm.click({ force: true });
+        await this.searchInput.fill(query);
+        await this.searchInput.press('Enter');
         await this.firstProductListing.click();
-      
-        await this.buyButton.waitFor({ state: 'visible' });
         await this.buyButton.click();
-      
-        await this.shoppingCartButton.waitFor({ state: 'visible' });
         await this.shoppingCartButton.click();
       }
 }
--- hw_18-PlayWright1/tests/promotest.spec.ts
+++ hw_18-PlayWright1/tests/promotest.spec.ts
@@ -1,30 +1,25 @@
 import {test, expect} from '@playwright/test';
-import { describe } from 'node:test';
-import { PromoPage, PromoPageElements } from 'page-modules/promo-page';
+import { PromoPage} from 'page-modules/promo-page';
 
 test.describe ('Promo Page Tests', () => {
     let promoPage: PromoPage;
-    let promoPageElements: PromoPageElements;
 
     test.beforeEach(async ({ page }) => {
         promoPage = new PromoPage(page);
-        promoPageElements = new PromoPageElements(page, page.context());
         promoPage.goTo();
     });
 
     test ('Search form should be visible', async ({ page }) => {
-        await expect(promoPageElements.searchInput).toBeVisible({ timeout: 30000 });
+        await expect(promoPage.searchInput).toBeVisible();
     });
 
     test ('Search products', async ({ page }) => {
-        await promoPageElements.inputValue('iphone');
-        await expect(promoPageElements.searchResultGallery).toBeVisible({ timeout: 30000 });
+        await promoPage.inputValue('iphone');
+        //await expect(promoPage.searchResultGallery);
     })
 
     test ('Add product to cart', async ({ page }) => {
-        await promoPageElements.inputValue('iphone');
-        await promoPageElements.addToCart();
-        await expect(promoPageElements.shoppingCartButton).toBeVisible({ timeout: 30000 });
+        await promoPage.addToCart('iphone');
     })
 
 })
